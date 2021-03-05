4a9d1aa898aadca976db6eb91e21a7da6ab32476
Daisuke Sasaki <67216632+daisuke2549@users.noreply.github.com>
2021-03-05 21:10:52 +0900

    配列の分割代入を学習

--- src/index.js
+++ src/index.js
@@ -1,40 +1,48 @@
-const name = "suzuki";
-const age = 30;
+// // const name = "suzuki";
+// // const age = 30;
 
-const message1 = "私の名前は" + name + "です。年齢は" + age;
-console.log(message1);
+// // const message1 = "私の名前は" + name + "です。年齢は" + age;
+// // console.log(message1);
 
-const message2 = `私の名前は${name}です。年齢は${age}です`;
-console.log(message2);
+// // const message2 = `私の名前は${name}です。年齢は${age}です`;
+// // console.log(message2);
 
-/* アロー関数 */
+// /* アロー関数 */
 
-// function func1(str) {
-//   return str;
-// }
+// // function func1(str) {
+// //   return str;
+// // }
 
-const func1 = function (str) {
-  return str;
-};
+// // const func1 = function (str) {
+// //   return str;
+// // };
 
-console.log(func1("func1です"));
+// // console.log(func1("func1です"));
 
-const func2 = (str) => str;
+// // const func2 = (str) => str;
 
-console.log(func2("func2です"));
+// // console.log(func2("func2です"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
+// // const func3 = (num1, num2) => {
+// //   return num1 + num2;
+// // };
 
-console.log(func3(50, 20));
+// // console.log(func3(50, 20));
 
-/* 分割代入 */
+// /* 分割代入 */
 
-const human = {
-  name: "suzuki",
-  age: 29
-};
+// const human1 = {
+//   name: "suzuki",
+//   age: 29
+// };
 
-const message = `名前は${human.name}です。年齢は${human.age}です`;
-console.log(message);
+// // const message = `名前は${human.name}です。年齢は${human.age}です`;
+// // console.log(message);
+
+// const { name, age } = human1;
+// const message2 = `名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+// const myProfile = ["suzuki", 48];
+// const message4 = `私の名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message4);
