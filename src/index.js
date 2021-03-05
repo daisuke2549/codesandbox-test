5bc919d23653f7e448a9fe36b85eb0e0e1a9ea52
Daisuke Sasaki <67216632+daisuke2549@users.noreply.github.com>
2021-03-05 20:45:53 +0900

    アロー関数について学習中

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import "./styles.css";
+const name = "suzuki";
+const age = 30;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const message1 = "私の名前は" + name + "です。年齢は" + age;
+console.log(message1);
+
+const message2 = `私の名前は${name}です。年齢は${age}です`;
+console.log(message2);
+
+/* アロー関数 */
+
+function func1(str) {
+  return str;
+}
+
+console.log(func1("func1です"));
